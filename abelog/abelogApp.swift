db73ed771644b6b0297adb44df14acc4b0f6ab51
Youngbin Choi <chldudqlsdl@naver.com>
2025-04-11 14:33:24 +0900

    Initial commit

---
+++ abelog/abelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  abelogApp.swift
+//  abelog
+//
+//  Created by 崔 瑩斌 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct abelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ abelogTests/abelogTests.swift
@@ -0,0 +1,17 @@
+//
+//  abelogTests.swift
+//  abelogTests
+//
+//  Created by 崔 瑩斌 on 4/11/25.
+//
+
+import Testing
+@testable import abelog
+
+struct abelogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
